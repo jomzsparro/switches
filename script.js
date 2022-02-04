352364990ff20acce40a00139bab150f3b9fe093
jomzsparro <80109495+jomzsparro@users.noreply.github.com>
2022-02-03 20:32:49 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,197 @@
+
+
+
+
+var lor1 = "The pain itself is love, the main storage system. Now from the wise compensation, the free sauce to do so, the cartoon. In order to look at some pain, it is important to be selected by the cat. In some now and the torturer's skirt, the arrows are set out.";
+var sel1 = "Select your payment method by clicking on the logo below"
+var cr1 = "Make cross-border payment-familiar for every customer."
+var del1 = "Deliver a seamless customer Expirience for international cardholders with 133 currencys available for any Visa, Mastercard,Amex or JVC transaction"
+var pay1 = "Pay by Credit Card"
+
+//french
+
+var lor2 = "La douleur elle-même est l'amour, le principal système de stockage. Maintenant de la compensation sage, la sauce gratuite pour le faire, le dessin animé. Afin de regarder certaines douleurs, il est important d'être sélectionné par le chat."
+var sel2 = "Choisissez votre moyen de paiement en cliquant sur le logo ci-dessous"
+var cr2 = "Rendre le paiement transfrontalier familier pour chaque client."
+var del2 = "Offrez une expérience client transparente aux titulaires de carte internationaux avec 133 devises disponibles pour toute transaction Visa, Mastercard, Amex ou JVC"
+var pay2 = "Payer par carte de crédit"
+
+// chinese
+
+var lor3 ="地球由土地、空氣、水和生命組成。 土地包含山脈、山谷和平坦的地區。 空氣由不同的氣體組成，主要是氮氣和氧氣。 水包括海洋、湖泊、河流、溪流、雨、雪和冰 空氣、水和生命組成。 土地包含山脈、山谷和平坦的地區。"
+var sel3 ="單擊下面的徽標選擇您的付款方式"
+var cr3 = "讓每一位客戶都熟悉跨境支付戶都熟悉跨戶都熟悉戶都熟悉跨。"
+var del3 = "為國際持卡人提供無縫的客戶體供無縫的客供無縫的客供無縫的客供無縫的客供無縫的客驗，133 種貨幣可用於任何 Visa、Mastercard、Amex 或 JVC 交易"
+var pay3 = "單擊下面的徽標選擇您的付款方式"
+
+//THAI
+var lor4 = "Lok prakxbd̂wy din xākāṣ̄ n̂ả læa chīwit thī̀din prakxbd̂wy p̣hūk̄heā h̄ubk̄heā læa phụ̄̂nthī̀ rāb xākāṣ̄ prakxbd̂wy ḱās h̄lāy chnid s̄̀wn h̄ıỵ̀ pĕn nịtorcen læa xxksicen n̂ả dị̂kæ̀ mh̄ās̄muthr thales̄āb mæ̀n̂ả lảṭhār f̄n h̄ima læa"
+var sel4 = "Leụ̄xk wiṭhī kār chảra ngein k̄hxng khuṇ doy khlik thī̀ lokô d̂ān l̀āng"
+var cr4 = "Thảkhwām khûnkhey kạb kār chảra ngein k̄ĥām phrmdæn h̄ı̂ kạb lūkkĥā thuk"
+var del4 ="Mxb pras̄bkārṇ̒ lūkkĥā thī̀ rị̂ rxy t̀x h̄ı̂ kạb p̄hū̂ t̄hụ̄x bạtr rah̄ẁāng pratheṣ̄ d̂wy 133 s̄kul ngein s̄ảh̄rạb kār thả ṭhurkrrm Visa, Mastercard, Amex h̄rụ̄x JVC"
+var pay4 ="C̀āy d̂wy bạtr kherdit"
+
+//Vietnamese
+
+var lor5 = "Trái đất bao gồm đất, không khí, nước và sự sống. Vùng đất có núi, thung lũng và các khu vực bằng phẳng. Không khí được tạo thành từ các khí khác nhau, chủ yếu là nitơ và oxy. Nước bao gồm đại dương, hồ, sông, suối, mưa, tuyết và băng"
+var sel5 = "Chọn phương thức thanh toán của bạn bằng cách nhấp vào biểu tượng bên dưới"
+var cr5 = "Làm cho thanh toán xuyên biên giới trở nên quen thuộc đối với mọi khách hàng."
+var del5= "Mang đến trải nghiệm khách hàng liền mạch cho chủ thẻ quốc tế với 133 loại tiền tệ khả dụng cho bất kỳ giao dịch Visa, Mastercard, Amex hoặc JVC nào"
+var pay5 = "Trả bằng thẻ tín dụng"
+
+//korean
+
+var lor6 = "jiguneun ttang, gong-gi, mul, saengmyeong-eulo ilueojyeo issseubnida. tojineun san, gyegog mich pyeongpyeonghan jiyeog-eul pohamhabnida. gong-gineun dayanghan giche, julo jilsowa sansolo guseongdoeeo issseubnida."
+var sel6 = "alae logoleul keullighayeo jibul bangbeob-eul seontaeghasibsio"
+var cr6 = "modeun gogaeg-ege guggyeong gan jibul-eul chinsughage mandeusibsio."
+var del6 = "modeun Visa, Mastercard, Amex ttoneun JVC geolaee sayonghal su issneun 133gae tonghwalo gugje kadeu sojija-ege wonhwalhan gogaeg gyeongheom jegong"
+var pay4 = "sin-yong kadeu gyeolj"
+
+// Japanese
+
+var lor7 = "地球は、土地、空気、水、そして生命で構成されています。 土地には山、谷、平坦な地域があります。 空気はさまざまなガス、主に窒素と酸素で構成されています。 水には、海、湖、川、小川、雨、雪、氷が含まれます"
+var sel7 = "下のロゴをクリックしてお支払い方法を選択してください"
+var cr7 = "すべての顧客に馴染みのある国境を越えた支払いを行います。"
+var del7 = "Visa、Mastercard、Amex、またはJVCの取引で利用可能な133通貨を使用して、国際的なカード所有者にシームレスなカスタマーエクスペリエンスを提供します"
+var pay7 = "クレジットカードで支払う"
+
+//Indonesian
+var lor8 = "Bumi terdiri dari tanah, udara, air dan kehidupan. Tanahnya berisi pegunungan, lembah dan daerah datar. Udara terdiri dari berbagai gas, terutama nitrogen dan oksigen. Airnya meliputi lautan, danau, sungai, aliran air, hujan, salju, dan es"
+var sel8 = "Pilih metode pembayaran Anda dengan mengklik logo di bawah ini"
+var cr8 = "Buat pembayaran lintas batas menjadi akrab bagi setiap pelanggan."
+var del8 = "Memberikan Pengalaman pelanggan yang lancar bagi pemegang kartu internasional dengan 133 mata uang yang tersedia untuk setiap transaksi Visa, Mastercard, Amex, atau JVC"
+var pay8 = "Bayar dengan kartu kredit"
+
+
+
+
+
+
+document.querySelector("#english").addEventListener("click",english);
+document.querySelector("#french").addEventListener("click",french);
+document.querySelector("#chinese").addEventListener("click",chinese);
+document.querySelector("#thai").addEventListener("click",thai);
+document.querySelector("#vietnamese").addEventListener("click",vietnamese);
+document.querySelector("#korean").addEventListener("click",korean);
+document.querySelector("#japanese").addEventListener("click",japanese);
+document.querySelector("#indonesian").addEventListener("click",indonesian);
+
+  function english(){
+      document.querySelector("#lorem").innerHTML = lor1;
+      document.querySelector("#select").innerHTML = sel1;
+    document.querySelector("#cross").innerHTML = cr1;
+    document.querySelector("#deliver").innerHTML = del1;
+    document.querySelector("#pay").innerHTML = pay1;
+  
+    
+      
+    }
+
+function french(){
+    document.querySelector("#lorem").innerHTML = lor2;
+    document.querySelector("#select").innerHTML = sel2;
+    document.querySelector("#cross").innerHTML = cr2;
+    document.querySelector("#deliver").innerHTML = del2;
+    document.querySelector("#pay").innerHTML = pay2;
+    
+}
+
+function chinese(){
+
+  document.querySelector("#lorem").innerHTML = lor3;
+    document.querySelector("#select").innerHTML = sel3;
+    document.querySelector("#cross").innerHTML = cr3;
+    document.querySelector("#deliver").innerHTML = del3;
+    document.querySelector("#pay").innerHTML = pay3;
+    
+}
+
+function thai(){
+
+  document.querySelector("#lorem").innerHTML = lor4;
+    document.querySelector("#select").innerHTML = sel4;
+    document.querySelector("#cross").innerHTML = cr4;
+    document.querySelector("#deliver").innerHTML = del4;
+    document.querySelector("#pay").innerHTML = pay4;
+    
+}
+function vietnamese(){
+  document.querySelector("#lorem").innerHTML = lor5;
+    document.querySelector("#select").innerHTML = sel5;
+    document.querySelector("#cross").innerHTML = cr5;
+    document.querySelector("#deliver").innerHTML = del5;
+    document.querySelector("#pay").innerHTML = pay5;
+    
+}
+
+function korean(){
+
+  document.querySelector("#lorem").innerHTML = lor6;
+    document.querySelector("#select").innerHTML = sel6;
+    document.querySelector("#cross").innerHTML = cr6;
+    document.querySelector("#deliver").innerHTML = del6;
+    document.querySelector("#pay").innerHTML = pay6;
+    
+}
+
+function japanese(){
+
+  document.querySelector("#lorem").innerHTML = lor7;
+    document.querySelector("#select").innerHTML = sel7;
+    document.querySelector("#cross").innerHTML = cr7;
+    document.querySelector("#deliver").innerHTML = del7;
+    document.querySelector("#pay").innerHTML = pay7;
+    
+}
+
+
+function indonesian(){
+
+  document.querySelector("#lorem").innerHTML = lor8;
+    document.querySelector("#select").innerHTML = sel8
+    document.querySelector("#cross").innerHTML = cr8;
+    document.querySelector("#deliver").innerHTML = del8;
+    document.querySelector("#pay").innerHTML = pay8;
+    
+}
+
+
+
+
+// const langEl = document.querySelector('.langWrap');
+// 		const link = document.querySelectorAll('a');
+// 		const titleEl = document.querySelector('.title');
+// 		const descrEl = document.querySelector('.description');
+
+// input.forEach(el => {
+//   el.addEventListener('click', () => {
+//     langEl.querySelector('.active').classList.remove('active');
+//     el.classList.add('active');
+
+//     const attr = el.getAttribute('language');
+
+//     titleEl.textContent = data[attr].title;
+//     descrEl.textContent = data[attr].description;
+//   });
+// });
+
+// var data = {
+//   "english": 
+//   {
+//     "title": "Hello World",
+//     "description": 
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non adipisci eligendi repellat ad dolor veritatis itaque sequi minus iste, doloremque. Officiis non eaque atque excepturi repudiandae nulla eos eligendi magni molestiae eius distinctio, voluptas pariatur incidunt et culpa inventore aspernatur recusandae nihil asperiores, vitae, maiores laborum quasi perspiciatis natus dignissimos! Accusantium aliquam nostrum impedit dignissimos iste, iure inventore! Sapiente, labore earum ut dicta ducimus asperiores laudantium natus officiis, quisquam placeat aspernatur voluptatum aut voluptates tenetur quos magni fugit quia. Fugiat."
+//   },
+//   "kazakh": 
+//   {
+//     "title": "Сәлем Әлем",
+//     "description": 
+//       "Сәбіз Lorem ipsum, жеңілдік. Бұл ауырсыну үшін осы ұннан таңдамаңыз, сондықтан аз Осы ауырсынуды орындаңыз. Қызметтер жоқ және оның айырмашылығын ұлы ыңғайсыздық таңдау, тәжірибе ретінде қабылданған ештеңе өңдеу үшін нәтиже инцидент және қателіктерді зерттеуші ләззат, өмір, encounter born сияқты үлкен тағамдар-бәрі! Осы мәселе бойынша біздің сабоның кейбірін ашу, зерттеуші дұрыс! Дана, біз asperiores туған қызметтер тізімі деді олардың жұмыс, кез келген уақытта, содан бері үлкен жүгірістер, соның ішінде ләззат немесе рахат өңдеу сұраймыз. Қашуға."
+//   },
+//   "japanese": 
+//   {
+//     "title": "ハロー・ワールド",
+//     "description": 
+//       "ﾂつｨﾂ知ﾂづｧﾂつｹﾂ-ﾂ新ﾂ陳ﾂ湘ｮﾂ陛ｱ これらの苦痛から真実の苦痛に選ぶために得てはいけない、従ってより少しにこの苦痛に従ってはいけない。 サービスではないと、彼の区別の大きな不快感から選択するそれらの否認を歓迎しない、結果として生じた事件の喜びと障害エクスプローラが受け入 このことについて私たちの下駄のいくつかを非難,右エクスプローラ! 的に仕事をしていまasperioresリストの生まれのサービスをつけてください編集喜びや楽しみの開催なども走りました。 走れ。."
+//   }
+// }
